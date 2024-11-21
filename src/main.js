19be4187b9952388cfb3382afcca59771d2f7e53
Nico Guillén <nicolasguillen@proton.me>
2024-11-21 20:46:38 +0100

    Move image out

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { resizeCanvas, listenToChanges } from "./window.js"
 const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("game_canvas"))
 
 const image = new Image();
-image.src = "/src/dvd_logo.png";
+image.src = "./assets/dvd_logo.png";
 image.onload = () => {
     listenToChanges(canvas, image);
     resizeCanvas(canvas, image);
